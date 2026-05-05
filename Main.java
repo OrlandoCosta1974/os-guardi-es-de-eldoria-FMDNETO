5dc32afb486cb3ff7f40aaab17e057a365b3c84b
Fernando Marques Duarte <marques.duarte@hotmail.com>
2026-05-04 22:07:47 -0300

    Desafio NOVATO do TEMA 1 - Criação de Personagens como objetos e exibição de seus atributos como estado

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
+//Criação da classe Main onde o programa irá inciar
 public class Main {
+    //Aqui definimos o método construtor da classe main
     public static void main(String[] args) {
+
+        //Criação dos objetos e seus atributos
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        Personagem heroi2 = new Personagem("Merlin", "Mago", 20, 100, 20);
+        Personagem heroi3 = new Personagem("Enéias", "Sábio", 56, 200, 100);
 
-        .
-        .
-        .
 
+        //Execução do método Exibir Status da classe personagem
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
+        heroi3.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,3 +1,4 @@
+//Criação da classe Personagem
 public class Personagem {
     String nome;
     String classe;
@@ -5,20 +6,22 @@ public class Personagem {
     int pontosDeVida;
     double poderBase;
 
+    //Criação do método construtor da classe Personagem
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
-
+    //Criação do método Exibir Status que mostra os atributos do objeto (estado)
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nivel: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
     }
 }
 
